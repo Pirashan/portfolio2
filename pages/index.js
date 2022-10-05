c0b5c557efc8bcba7bf01f6fa05301852892a608
Pirashan <pirashan99@gmail.com>
2022-10-04 20:17:55 -0400

    About page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Work from "./components/Work";
 
 export default function Home() {
   return (
