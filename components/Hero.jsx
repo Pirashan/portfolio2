deece929f15fbb56af6725779c6b1468fc5fd35c
Pirashan <pirashan99@gmail.com>
2022-10-10 23:31:48 -0400

    test

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div id='hero' className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                {/* <p className='uppercase text-sm tracking-widest text-gray-600'>
+                <p className='uppercase text-sm tracking-widest text-gray-600'>
                     LET&#39;S BUILD SOMETHING TOGETHER
                 </p>
                 <h1 className='py-4 text-gray-700'>
@@ -22,7 +22,7 @@ const Hero = () => {
                     I&#39;m a front-end web developer specializing in building exceptional
                     digital experiences. Currently, I&#39;m focused on building responsive
                     front-end web applications while learning back-end technologies.
-                </p> */}
+                </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
 
                     <a
