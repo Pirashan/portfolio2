96a078d420bef0f6f0138b89bf54f33f0bc215ae
Pirashan <pirashan99@gmail.com>
2024-05-20 19:01:27 -0400

    added graphql project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import spotifyImg from '../public/assets/spotifybuild.JPG'
-import gymImg from '../public/assets/gymapp.jpeg'
+import notesImg from '../public/assets/notes.png'
 import TodoImg from '../public/assets/Todo.jpeg'
 import crcImg from '../public/assets/crcgreenproject.png'
 import appleredesignImg from '../public/assets/appleredesign.png'
@@ -59,6 +59,16 @@ const Projects = () => {
                 <h3 className='text-2xl text-white tracking-wider text-center'>Todo App</h3>
                 <p className='pb-4 pt-2 text-white text-center'>Next.js, GPT-4 API, Zustand, Appwrite</p>
                 <a href="https://todo-puce-one.vercel.app/" rel="noopener">
+                    <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:bg-gray-600 hover:text-white'>More Info</p>
+                </a>
+            </div>
+        </div>
+        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+            <Image className='rounded-xl group-hover:opacity-10' src={notesImg} alt='/' />
+            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+                <h3 className='text-2xl text-white tracking-wider text-center'>Notes App</h3>
+                <p className='pb-4 pt-2 text-white text-center'>GraphQL, Prisma, Vita</p>
+                <a href="https://notetaker-graphql-frontend.onrender.com/" rel="noopener">
                     <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:bg-gray-600 hover:text-white'>More Info</p>
                 </a>
             </div>
