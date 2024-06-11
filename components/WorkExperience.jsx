7ea266452b57f8745d9b89b6a3860252be9bdcec
Pirashan <pirashan99@gmail.com>
2024-06-10 20:33:52 -0700

    added basic content to work experience timeline

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -17,88 +17,87 @@ function WorkExperience() {
         <VerticalTimeline>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            contentStyle={{ background: 'rgb(204, 204, 245)'}}
             contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-            date="2011 - present"
+            date="Mar 2023 - Present"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FaBriefcase />}
         >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <h2 className="vertical-timeline-element-title">Business Analyst</h2>
+            <h4 className="vertical-timeline-element-subtitle">BTCG</h4>
             <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                - Executed lead and analyst roles for an SAP EAM implementation project to
+                provide resource support for utility industry clients
+            </p>
+            <p>
+                - Pitched a TensorFlow-based ANN to estimate time to repair damaged assets
+            </p>
+            <p>
+                - Led analysis and migration on asset data, including data cleaning,
+                transformation, and validation while managing analysts to run mock loads and
+                data verification/validation cycles
             </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2010 - 2011"
+            date="Aug 2022 - Feb 2023"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FaBriefcase />}
         >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+            <h2 className="vertical-timeline-element-title">Salesforce Business Analyst</h2>
+            <h4 className="vertical-timeline-element-subtitle">Adi Group</h4>
+            {/* <p>
+                - Deployed Service Cloud from ideation to implementation, including roadmap
+                development, UAT and training of end users
             </p>
+            <p>
+                - Designed Visualforce pages using Apex and JS to better handle case creation,
+                decreasing cycle time by 65%
+            </p>
+            <p>
+                - Created dashboards for KPI tracking using APIs in Power BI from ProCore
+                data, leading to reduction in average ticket age by 40%
+            </p> */}
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2008 - 2010"
+            date="May 2021 - Jan 2022"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FaBriefcase />}
         >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-            <p>
-            User Experience, Visual Design
-            </p>
+            <h2 className="vertical-timeline-element-title">Customer Success Engineer</h2>
+            <h4 className="vertical-timeline-element-subtitle">Shoplogix Inc.</h4>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2006 - 2008"
+            date="Jan 2019 - Apr 2019"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FaBriefcase />}
         >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-            User Experience, Visual Design
-            </p>
+            <h2 className="vertical-timeline-element-title">Project Manager</h2>
+            <h4 className="vertical-timeline-element-subtitle">Toyota Motor Manufacturing Canada</h4>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="April 2013"
+            date="Sep 2024 - Present"
             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
             icon={<IoMdSchool />}
         >
-            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-            <p>
-            Strategy, Social Media
-            </p>
+            <h2 className="vertical-timeline-element-title">Georgia Institute of Technology</h2>
+            <h3 className="vertical-timeline-element-title">MS in Computer Science</h3>
+            <h4 className="vertical-timeline-element-subtitle">Atlanta, GA</h4>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="November 2012"
+            date="2017 - 2022"
             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
             icon={<IoMdSchool />}
         >
-            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+            <h2 className="vertical-timeline-element-title">University of Waterloo</h2>
+            <h3 className="vertical-timeline-element-title">BASc in Chemical Engineering</h3>
+            <h4 className="vertical-timeline-element-subtitle">Waterloo, ON</h4>
             <p>
-            Creative Direction, User Experience, Visual Design
-            </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="2002 - 2006"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<IoMdSchool />}
-        >
-            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-            <p>
-            Creative Direction, Visual Design
+            Cum. GPA: 3.70 / 4.0
             </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
